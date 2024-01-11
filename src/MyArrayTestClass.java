3840e499f9511fb3415ef67e478d3319d9270760
shayla <shayla8002@gmail.com>
2024-01-11 13:19:56 -0800

    sdfs

--- src/MyArrayTestClass.java
+++ src/MyArrayTestClass.java
@@ -1,5 +1,38 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class MyArrayTestClass
+{
+    public static void main(String[] args)
+    {
+        int[] arrOfInts = {1,2,3,4,5};
+        System.out.println("-------- First Output --------");
+        for (int i: arrOfInts)
+        {
+            System.out.print(i + " ");
+        }
+
+        addToNums(arrOfInts);
+
+        System.out.println();
+        System.out.println("-------- Second Output --------");
+        for (int i: arrOfInts)
+        {
+            System.out.print(i + " ");
+        }
+
+        addToNums(arrOfInts);
+
+        System.out.println();
+        System.out.println("-------- Shayla's Third Output --------");
+        for (int i: arrOfInts)
+        {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void addToNums(int arr1[])
+    {
+        for (int i = 0; i < arr1.length; i++)
+        {
+            arr1[i] += 2;
+        }
     }
 }
